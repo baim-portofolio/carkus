ac3c7183f5f3d126587a4bfb2cba251e9a214367
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-06-09 14:39:46 +0700

    feat: authorization 'campus/create'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
+
 import { AuthModule } from './auth/auth.module';
 import { CampusModule } from './campus/campus.module';
-
+import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
+import { RolesGuard } from './auth/role/roles.guard';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { env } from 'process';
+import { PassportModule } from '@nestjs/passport';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule, CampusModule],
--- src/campus/campus.module.ts
+++ src/campus/campus.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CampusService } from './campus.service';
 import { CampusController } from './campus.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { RolesGuard } from '../auth/role/roles.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [],
   controllers: [CampusController],
-  providers: [CampusService, PrismaService]
+  providers: [
+    CampusService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    JwtService,
+  ],
 })
 export class CampusModule {}
