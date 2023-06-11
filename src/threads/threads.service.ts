c6b216236b1d7e703e29d1893c13bc3070ce37f8
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-06-11 11:47:12 +0700

    feat: try-catch

--- src/threads/threads.service.ts
+++ src/threads/threads.service.ts
@@ -2,70 +2,95 @@ import { Injectable } from '@nestjs/common';
 import { CreateThreadDto } from './dto/create-thread.dto';
 import { UpdateThreadDto } from './dto/update-thread.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 @Injectable()
 export class ThreadsService {
   constructor(private prisma: PrismaService) {}
 
   async create(id_campus: string, createThreadDto: CreateThreadDto, req) {
-    const createThread = await this.prisma.threads.create({
-      data: {
-        ...createThreadDto,
-
-        campus: {
-          connect: {
-            id: id_campus,
+    try {
+      const createThread = await this.prisma.threads.create({
+        data: {
+          ...createThreadDto,
+          campus: {
+            connect: {
+              id: id_campus,
+            },
+          },
+          user: {
+            connect: {
+              id: req.id_user,
+            },
           },
         },
+      });
 
-        user: {
-          connect: {
-            id: req.id_user,
-          },
-        },
-      },
-    });
-
-    return createThread;
+      return createThread;
+    } catch (error) {
+      throw new HttpException(
+        'Error while creating thread',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findAll(id_campus: string) {
-    const resultThreads = await this.prisma.threads.findMany({
-      where: {
-        id_campus: id_campus,
-      },
-      include: {
-        user: {
-          select: {
-            username: true,
-            role: true,
+    try {
+      const resultThreads = await this.prisma.threads.findMany({
+        where: {
+          id_campus: id_campus,
+        },
+        include: {
+          user: {
+            select: {
+              username: true,
+              role: true,
+            },
           },
         },
-      },
-    });
-    return resultThreads;
+      });
+
+      return resultThreads;
+    } catch (error) {
+      throw new HttpException(
+        'Error while finding threads',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async update(id_thread:string, updateThreadDto: UpdateThreadDto) {
-    const updateThread = await this.prisma.threads.update({
-      where: {
-        id: id_thread,
-      },
-      data: {
-        ...updateThreadDto,
-      },
-    });
+  async update(id_thread: string, updateThreadDto: UpdateThreadDto) {
+    try {
+      const updateThread = await this.prisma.threads.update({
+        where: {
+          id: id_thread,
+        },
+        data: {
+          ...updateThreadDto,
+        },
+      });
 
-    return updateThread;
-        
+      return updateThread;
+    } catch (error) {
+      throw new HttpException(
+        'Error while updating thread',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async remove(id_thread: string) {
-    const removeThread = await this.prisma.threads.delete({
-      where: {
-        id: id_thread,
-      },
-    });
-
-    return removeThread;
+    try {
+      const removeThread = await this.prisma.threads.delete({
+        where: {
+          id: id_thread,
+        },
+      });
+  
+      return removeThread;
+    } catch (error) {
+      throw new HttpException('Error while removing thread', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
+  
 }
