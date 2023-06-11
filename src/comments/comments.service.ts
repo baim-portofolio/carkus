fdc4143b2a90ce941ac7cc566cd20ec8e07b273d
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-06-11 11:40:27 +0700

    feat: try-catch comments

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -2,71 +2,95 @@ import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 @Injectable()
 export class CommentsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(
-    createCommentDto: CreateCommentDto,
-    id_campus: string,
-    id_thread: string,
-    req,
-  ) {
-    const createComment = await this.prisma.comments.create({
-      data: {
-        ...createCommentDto,
-        user: {
-          connect: {
-            id: req.id_user,
+  async create(createCommentDto: CreateCommentDto, id_thread: string, req) {
+    try {
+      const createComment = await this.prisma.comments.create({
+        data: {
+          ...createCommentDto,
+          user: {
+            connect: {
+              id: req.id_user,
+            },
+          },
+          thread: {
+            connect: {
+              id: id_thread,
+            },
           },
         },
-        thread: {
-          connect: {
-            id: id_thread,
+      });
+
+      return createComment;
+    } catch (error) {
+      throw new HttpException(
+        'Error while creating comment',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findAll(id_thread: string) {
+    try {
+      const resultComments = await this.prisma.comments.findMany({
+        where: {
+          id_thread: id_thread,
+        },
+        include: {
+          user: {
+            select: {
+              username: true,
+              role: true,
+            },
           },
         },
-      },
-    });
+      });
 
-    return createComment;
+      return resultComments;
+    } catch (error) {
+      throw new HttpException(
+        'Error while finding comments',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async findAll(id_campus: string, id_thread: string) {
-    const resultComments = await this.prisma.comments.findMany({
-      where: {
-        id_thread: id_thread,
-      },
-      include: {
-        user: {
-          select: {
-            username: true,
-            role: true,
-          },
+  async update(id_comment: string, updateCommentDto: UpdateCommentDto) {
+    try {
+      const updateComment = await this.prisma.comments.update({
+        where: {
+          id: id_comment,
         },
-      },
-    });
+        data: updateCommentDto,
+      });
 
-    return resultComments;
+      return updateComment;
+    } catch (error) {
+      throw new HttpException(
+        'Error while updating comment',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async update(id_campus: string, id_thread: string, id_comment: string, updateCommentDto: UpdateCommentDto) {
-    const updateComment = await this.prisma.comments.update({
-      where: {
-        id: id_comment,
-      },
-      data: updateCommentDto,
-    });
+  async remove(id_comment: string) {
+    try {
+      const deleteComment = await this.prisma.comments.delete({
+        where: {
+          id: id_comment,
+        },
+      });
 
-    return updateComment;
-  }
-
-  async remove(id_campus: string, id_thread: string, id_comment: string) {
-    const deleteComment = await this.prisma.comments.delete({
-      where: {
-        id: id_comment,
-      },
-    });
-
-    return deleteComment;
+      return deleteComment;
+    } catch (error) {
+      throw new HttpException(
+        'Error while deleting comment',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
