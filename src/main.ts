2ddbd6439825038a09859e9e422ba3d464fc9974
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-06-20 16:19:53 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser('secret'));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
